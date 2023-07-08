1b780211473289eb646315bc4f785dbecc6db6dd
Pruthvik-Reddy <pruthvikreddy.21@gmail.com>
2023-07-08 00:45:38 -0400

    testing the time to get nodes

--- get_nodes_from_csv.py
+++ get_nodes_from_csv.py
@@ -9,3 +9,4 @@ filtered_data = data[data.iloc[:, 1] == 'car']
 filtered_data_2 = data[data.iloc[:, 2] == 'car']
 filtered_data.to_csv("english_assertions.csv")
 combined_df = pd.concat([filtered_data, filtered_data_2], axis=0)
+combined_df.to_csv("test_car.csv")
