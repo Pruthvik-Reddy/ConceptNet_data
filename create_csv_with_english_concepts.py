c7af52f24461985b7df94788d84d6fec06b05c90
Pruthvik-Reddy <pruthvikreddy.21@gmail.com>
2023-07-08 00:28:30 -0400

    Create csv with only english nodes

--- create_csv_with_english_concepts.py
+++ create_csv_with_english_concepts.py
@@ -20,10 +20,10 @@ only_english = []
 with open("conceptnet-assertions.csv", encoding="utf8") as f:
     for line in f.readlines():
         ls = line.split('\t')
-        if ls[1].startswith('/c/en/') and ls[2].startswith('/c/en/'):
-            rel = ls[0].split("/")[-1].lower()
-            head = del_pos(ls[1]).split("/")[-1].lower()
-            tail = del_pos(ls[2]).split("/")[-1].lower()
+        if ls[2].startswith('/c/en/') and ls[3].startswith('/c/en/'):
+            rel = ls[1].split("/")[-1].lower()
+            head = del_pos(ls[2]).split("/")[-1].lower()
+            tail = del_pos(ls[3]).split("/")[-1].lower()
 
             if not head.replace("_", "").replace("-", "").isalpha():
                 continue
@@ -32,7 +32,7 @@ with open("conceptnet-assertions.csv", encoding="utf8") as f:
                 continue
 
             only_english.append("\t".join([rel, head, tail]))
-
+print(len(only_english))
 with open("english_assertions.csv", "w", encoding="utf8") as f:
     f.write("\n".join(only_english))
 
