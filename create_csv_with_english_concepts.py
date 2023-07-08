77d4525fdd36717ea827f45875807034216f79ef
Pruthvik-Reddy <pruthvikreddy.21@gmail.com>
2023-07-08 00:01:57 -0400

    Create csv with only english nodes

--- create_csv_with_english_concepts.py
+++ create_csv_with_english_concepts.py
@@ -4,7 +4,7 @@ import pandas as pd
 #data=pd.read_csv("conceptnet-assertions.csv",header=None,delimeter="\t")
 
 #data.columns=["edge","start_node","end_node","other"]
-data = pd.read_csv('conceptnet-assertions.csv', delim_whitespace=True, header=None)
+data = pd.read_csv('conceptnet-assertions.csv', delim_whitespace=True, header=None,error_bad_lines=False)
 
 # Filter rows based on the second column
 filtered_data = data[data.iloc[:, 1].str.split('/').str[1] == 'en']
