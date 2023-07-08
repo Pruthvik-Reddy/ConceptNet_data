0bd022d6f7b746b3abcc1c7f02b07e52d1d43687
Pruthvik-Reddy <pruthvikreddy.21@gmail.com>
2023-07-08 00:13:06 -0400

    Create csv with only english nodes

--- create_csv_with_english_concepts.py
+++ create_csv_with_english_concepts.py
@@ -1,13 +1,39 @@
 import csv
 import pandas as pd
+import configparser
+import json
 
-#data=pd.read_csv("conceptnet-assertions.csv",header=None,delimeter="\t")
 
-#data.columns=["edge","start_node","end_node","other"]
-data = pd.read_csv('conceptnet-assertions.csv', delim_whitespace=True, header=None,error_bad_lines=False)
 
-# Filter rows based on the second column
-filtered_data = data[data.iloc[:, 1].str.split('/').str[1] == 'en']
+def del_pos(s):
+    """
+    Deletes part-of-speech encoding from an entity string, if present.
+    :param s: Entity string.
+    :return: Entity string with part-of-speech encoding removed.
+    """
+    if s.endswith("/n") or s.endswith("/a") or s.endswith("/v") or s.endswith("/r"):
+        s = s[:-2]
+    return s
 
-filtered_data = filtered_data[filtered_data.iloc[:, 2].str.split('/').str[1] == 'en']
-filtered_data.to_csv("english_assertions.csv")
+
+only_english = []
+with open("conceptnet-assertions.csv", encoding="utf8") as f:
+    for line in f.readlines():
+        ls = line.split('\t')
+        if ls[1].startswith('/c/en/') and ls[2].startswith('/c/en/'):
+            rel = ls[0].split("/")[-1].lower()
+            head = del_pos(ls[1]).split("/")[-1].lower()
+            tail = del_pos(ls[2]).split("/")[-1].lower()
+
+            if not head.replace("_", "").replace("-", "").isalpha():
+                continue
+
+            if not tail.replace("_", "").replace("-", "").isalpha():
+                continue
+
+            only_english.append("\t".join([rel, head, tail]))
+
+with open("english_assertions.csv", "w", encoding="utf8") as f:
+    f.write("\n".join(only_english))
+
+
