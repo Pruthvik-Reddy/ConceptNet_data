1ad23c3b301912c7b8ebfd8d94c0797d6ca06ec9
Pruthvik-Reddy <pruthvikreddy.21@gmail.com>
2023-07-10 10:49:50 -0400

    getting relavant concepts

--- relevant_concepts.py
+++ relevant_concepts.py
@@ -139,3 +139,64 @@ for i in range(len(potential_words)):
 sorted_items = sorted(concepts_cosine_sim.items(), key=lambda x: x[1], reverse=True)
 top_k_items = sorted_items[:6]
 print(top_k_items)
+
+
+
+
+
+
+
+
+
+edges=get_all_edges_of_a_concept("journey")
+related_nodes=[]
+for edge in edges:
+  lang1=edge["start"]["@id"].split("/")[2]
+  lang2=edge["end"]["@id"].split("/")[2]
+  if lang1=="en" and lang2=="en" :
+    if edge["start"]["label"]=="journey":
+      related_nodes.append(edge["end"]["label"])
+      #print(edge["end"]["label"],edge["rel"]["label"])
+    else:
+      related_nodes.append(edge["start"]["label"])
+      #print(edge["start"]["label"],edge["rel"]["label"])
+stop_words = ['a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he', 'in', 'is', 'it', 'its', 
+              'of', 'on', 'that', 'the', 'to', 'was', 'were', 'will', 'with']
+stop_words=set(stop_words)
+
+potential_words=[]
+for i in range(len(related_nodes)):
+   word=related_nodes[i]
+   words=word.split(" ")
+   for j in words:
+      if j not in stop_words:
+         potential_words.append(j)
+   potential_words.append(word)
+
+
+start_node_embedding = embeddings["journey"]
+end_node_embedding = embeddings["graduation"]
+
+end_node_embedding=[a + b for a, b in zip(start_node_embedding, end_node_embedding)]
+
+
+concepts_cosine_sim = {}
+for i in range(len(potential_words)):
+    word = potential_words[i]
+    words = word.split(" ")
+    if len(words) == 1:
+        if word in embeddings:
+            concepts_cosine_sim[word] = cosine_similarity([embeddings[word]], [end_node_embedding])[0][0]
+    
+    else:
+        embedding_sum = [0] * len(end_node_embedding)
+        for j in range(len(words)):
+            if words[j] in embeddings:
+               #embedding_sum+=embeddings[words[j]]
+                embedding_sum = [a + b for a, b in zip(embedding_sum, embeddings[words[j]])]
+        embedding_sum = [val / len(words) for val in embedding_sum]
+        concepts_cosine_sim[word] = cosine_similarity([embedding_sum], [end_node_embedding])[0][0]
+
+sorted_items = sorted(concepts_cosine_sim.items(), key=lambda x: x[1], reverse=True)
+top_k_items = sorted_items[:6]
+print(top_k_items)
