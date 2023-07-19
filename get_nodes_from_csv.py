5669695ab104eab0754e4a1a05773fa5122cad68
Pruthvik-Reddy <pruthvikreddy.21@gmail.com>
2023-07-19 00:52:53 -0400

    Checking direct links

--- get_nodes_from_csv.py
+++ get_nodes_from_csv.py
@@ -32,8 +32,8 @@ for i in range(len(metaphoric_pairs)):
   filtered_data = data[data.iloc[:, 1] == concept1]
   filtered_data_2 = data[data.iloc[:, 2] == concept1]
   combined_df = pd.concat([filtered_data, filtered_data_2], axis=0)
-  end_exists = data["start_node"].str.contains(concept2, case=False) | data["end_node"].str.contains(concept2, case=False)
-  if end_exists:
+  end_exists = combined_df["start_node"].str.contains(concept2, case=False) | combined_df["end_node"].str.contains(concept2, case=False)
+  if end_exists.any():
      positive_metaphoric_pairs_count+=1
      print("Positive : ", concept1,concept2)
   else:
